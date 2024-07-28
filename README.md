fae4009d1b62fad5fa3da1f3de3dd54f1ac1f92b
Branden Ciranni <branden.ciranni@gmail.com>
2024-07-28 00:05:37 -0400

    update instructions

--- README.md
+++ README.md
@@ -6,16 +6,48 @@ https://synonym.streamlit.app/
 
 ![synonym_gif](https://github.com/user-attachments/assets/b66939de-4c61-4600-b602-118f7b85c8dc)
 
-This app will be updated in real time as updates are made to this github repository! Right now, you can:
+This app will be updated in real time as updates are made to this github repository!
 
-1. Upload a left and a right dataset
-2. Provide pandas read options in a standard python dictionary
-3. Compare the Datasets
-4. Download the results!
-     - Hover over the top right of the table - there is a download button!
-  
-Future State:
+Synonym is a tool to help you compare datasets. It can help you identify differences between two datasets, and help you understand why those differences exist.
+        
+### How to use Synonym
 
-1. Select Key Columns in Each Dataset
-2. More Comprehensive Comparisons Rather than just the simple diff
-3. Thresholds for Numeric Column Comparisons
+1. **Load Datasets**:
+     - Upload two datasets you would like to compare.
+     - You can upload datasets in CSV or Excel format.
+
+2. **Supply Read Options**
+     - If your dataset requires special pandas read options, you can supply them in JSON format.
+     - This is useful if your dataset has a non-standard delimiter, encoding, or other special requirements.
+
+3. **Compare Datasets**
+     - Click the "Compare datasets" button to compare the two datasets.
+     - Select the **Key** columns to compare the datasets by.
+          - This could be a unique identifier, such as a:
+               
+               - `user_id`
+               - `order_id`
+               - `transaction_id`
+               - `department_id`
+          
+               or maybe a combination of columns, such as:
+
+               - `first_name` and `last_name`
+               - `street_address` and `city`
+               - `product_name` and `product_category`
+               - `product_id` and `department_id`
+          
+               anything that uniquely identifies a row in the dataset.
+          - Provide those columns, otherwise we'll look in the dataset order, row by row.
+          - **It is Highly recommended to provide join columns**.
+     - Synonym will compare the datasets column by column and row by row.
+     - If there are any differences, Synonym will explain why they exist.
+
+4. **Interpret Results**
+     - Synonym will show you the differences between the datasets, in a set of 12 different metrics.
+     - If the datasets are identical, Synonym will let you know.
+     - If the datasets are not identical, Synonym will explain why.
+
+5. **Export Results**
+     - You can export the results of the comparison to a CSV file.
+     - Hover over the top right corner of the comparison results and click the download button.
--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,54 @@ st.set_page_config(page_title='Synonym', page_icon=':ghost:', layout='wide', ini
 st.title(':ghost: Synonym')
 st.subheader('Stop seeing ghosts, start seeing data!')
 
-st.markdown('''
-:construction: **Under Construction** :construction:
+with st.expander('How to use Synonym'):
+    st.markdown('''
+    Synonym is a tool to help you compare datasets. It can help you identify differences between two datasets, and help you understand why those differences exist.
+        
+    ### How to use Synonym
 
-This app is still in the works. We're working hard to let you compare datasets with ease. :hammer_and_wrench:
-''')
+    1. **Load Datasets**:
+        - Upload two datasets you would like to compare.
+        - You can upload datasets in CSV or Excel format.
+
+    2. **Supply Read Options**
+        - If your dataset requires special pandas read options, you can supply them in JSON format.
+        - This is useful if your dataset has a non-standard delimiter, encoding, or other special requirements.
+
+    3. **Compare Datasets**
+        - Click the "Compare datasets" button to compare the two datasets.
+        - Select the **Key** columns to compare the datasets by.
+            - This could be a unique identifier, such as a:
+                
+                - `user_id`
+                - `order_id`
+                - `transaction_id`
+                - `department_id`
+            
+                or maybe a combination of columns, such as:
+
+                - `first_name` and `last_name`
+                - `street_address` and `city`
+                - `product_name` and `product_category`
+                - `product_id` and `department_id`
+            
+                anything that uniquely identifies a row in the dataset.
+            - Provide those columns, otherwise we'll look in the dataset order, row by row.
+            - **It is Highly recommended to provide join columns**.
+        - Synonym will compare the datasets column by column and row by row.
+        - If there are any differences, Synonym will explain why they exist.
+
+    4. **Interpret Results**
+        - Synonym will show you the differences between the datasets, in a set of 12 different metrics.
+        - If the datasets are identical, Synonym will let you know.
+        - If the datasets are not identical, Synonym will explain why.
+
+    5. **Export Results**
+        - You can export the results of the comparison to a CSV file.
+        - Hover over the top right corner of the comparison results and click the download button.
+
+    If you encounter any issues, please let us know by opening an issue on our [GitHub repository](https://github.com/branden-ciranni/synonym).
+    ''')
 
 st.markdown('---')
 st.header('Load Datasets')
@@ -45,10 +88,10 @@ def compare_datasets(key1: str, key2: str) -> None:
     df1 = st.session_state.get(f'df__{key1}')
     df2 = st.session_state.get(f'df__{key2}')
 
-    provide_join_on_cols = st.checkbox('Provide columns to join on?', key='join_on_cols_checkbox')
+    provide_join_on_cols = st.checkbox('Provide Key Columns to Join on?', key='join_on_cols_checkbox')
     if provide_join_on_cols:
         all_cols = set(df1.columns).union(set(df2.columns))
-        join_cols = st.multiselect('Columns to join on', list(all_cols), key='join_on_cols_multiselect')
+        join_cols = st.multiselect('Key Columns to Join on', list(all_cols), key='join_on_cols_multiselect')
     else:
         join_cols = None
 
