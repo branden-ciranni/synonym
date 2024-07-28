54e361e961a5c70f951e8d7436f848b29cb27394
Branden Ciranni <branden.ciranni@gmail.com>
2024-07-27 23:29:09 -0400

    added explanations to columns + improved row-by-row comparison

--- src/comparisons.py
+++ src/comparisons.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from typing import Any, Callable, Dict, List
+from typing import Any, Callable, Dict, List, Optional
 
 
 def _compare(metric_name: str, metric_calc: Callable[[pd.DataFrame], Any], df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
@@ -13,14 +13,38 @@ def _compare(metric_name: str, metric_calc: Callable[[pd.DataFrame], Any], df1: 
     calc2 = metric_calc(df2)
 
     if isinstance(calc1, pd.Series):
-        comparison = calc1.equals(calc2)
+        df1_cols = calc1.index.tolist()
+        df2_cols = calc2.index.tolist()
+        all_cols = list(set(df1_cols + df2_cols))
+        
+        explanation = ['**Explanation**']
+        comparison = True
+
+        for col in all_cols:
+            val1 = calc1.get(col)
+            val2 = calc2.get(col)
+            if val1 is None:
+                comparison = False
+                explanation.append(f'- {col}: {val2} (only in right)')
+            elif val2 is None:
+                comparison = False
+                explanation.append(f'- {col}: {val1} (only in left)')
+            elif val1 != val2:
+                comparison = False
+                explanation.append(f'- {col}: {val1} (left) != {val2} (right)')
+            else:
+                explanation.append(f'- {col}: {val1} (left) == {val2} (right)')
+
+        explanation = '\n'.join(explanation)
     else:
         comparison = calc1 == calc2
+        explanation = f'**Explanation**\n- {calc1} (left) == {calc2} (right)' if comparison else f'**Explanation**\n- {calc1} (left) != {calc2} (right)'
     return {
         'metric': metric_name,
         'result1': calc1,
         'result2': calc2,
         'comparison': comparison,
+        'explanation': explanation,
     }
     
 
@@ -37,8 +61,8 @@ def shapes(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='shape',
-        metric_calc=lambda df: f'{df.shape[0]} rows, {df.shape[1]} columns',
+        metric_name='Shapes',
+        metric_calc=lambda df: {'rows': df.shape[0], 'columns': df.shape[1]},
         df1=df1,
         df2=df2,
     )
@@ -56,7 +80,7 @@ def columns(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='columns',
+        metric_name='Columns',
         metric_calc=lambda df: df.columns.tolist(),
         df1=df1,
         df2=df2,
@@ -75,8 +99,8 @@ def dtypes(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='dtypes',
-        metric_calc=lambda df: df.dtypes,
+        metric_name='Data Types',
+        metric_calc=lambda df: df.dtypes.rename('dtype').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -94,8 +118,8 @@ def nulls(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='nulls',
-        metric_calc=lambda df: df.isnull().sum(),
+        metric_name='Nulls',
+        metric_calc=lambda df: df.isnull().sum().rename('nulls').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -113,7 +137,7 @@ def memory_usages(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='memory_usage',
+        metric_name='Memory Usage',
         metric_calc=lambda df: df.memory_usage(deep=True).sum(),
         df1=df1,
         df2=df2,
@@ -132,8 +156,8 @@ def counts(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='count',
-        metric_calc=lambda df: df.count(),
+        metric_name='Count',
+        metric_calc=lambda df: df.count().rename('count').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -151,8 +175,8 @@ def unique_counts(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='unique_count',
-        metric_calc=lambda df: df.nunique(),
+        metric_name='Unique Count',
+        metric_calc=lambda df: df.nunique().rename('unique_count').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -170,8 +194,8 @@ def means(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='mean',
-        metric_calc=lambda df: df.mean(numeric_only=True),
+        metric_name='Mean',
+        metric_calc=lambda df: df.mean(numeric_only=True).rename('mean').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -189,8 +213,8 @@ def medians(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='median',
-        metric_calc=lambda df: df.median(numeric_only=True),
+        metric_name='Median',
+        metric_calc=lambda df: df.median(numeric_only=True).rename('median').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -208,8 +232,8 @@ def stds(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='std',
-        metric_calc=lambda df: df.std(numeric_only=True),
+        metric_name='Standard Deviation',
+        metric_calc=lambda df: df.std(numeric_only=True).rename('standard deviation').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -227,8 +251,8 @@ def mins(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='min',
-        metric_calc=lambda df: df.min(numeric_only=True),
+        metric_name='Min',
+        metric_calc=lambda df: df.min(numeric_only=True).rename('min').rename_axis('column'),
         df1=df1,
         df2=df2,
     )
@@ -246,8 +270,8 @@ def maxes(df1: pd.DataFrame, df2: pd.DataFrame) -> Dict[str, Any]:
     :rtype: Dict[str, Any]
     """
     return _compare(
-        metric_name='max',
-        metric_calc=lambda df: df.max(numeric_only=True),
+        metric_name='Max',
+        metric_calc=lambda df: df.max(numeric_only=True).rename('max').rename_axis('column'),
         df1=df1,
         df2=df2,
     )    
@@ -279,4 +303,58 @@ def compare_all(df1: pd.DataFrame, df2: pd.DataFrame) -> List[Dict[str, Any]]:
         maxes(df1, df2),
     ]
 
+def compare_row_by_row(df1: pd.DataFrame, df2: pd.DataFrame, join_cols: Optional[List[str]] = None) -> pd.DataFrame:
+    """Compare two DataFrames row by row.
 
+    Do a full outer join, and show, row by row, where there are differences column by column.
+    Only the rows that are different will be shown.
+
+    If the key is None, use the index.
+    
+    :param df1: The first DataFrame.
+    :type df1: pd.DataFrame
+    
+    :param df2: The second DataFrame.
+    :type df2: pd.DataFrame
+
+    :param join_cols: The columns to join on.
+    :type join_cols: List[str]
+    
+    :return: A DataFrame containing the comparison results.
+    :rtype: pd.DataFrame
+    """
+    if join_cols is None:
+        merged = pd.merge(
+            df1, df2, left_index=True, right_index=True, how='outer', suffixes=('_left', '_right')
+        )
+    else:
+        merged = pd.merge(
+            df1, df2, on=join_cols, how='outer', suffixes=('_left', '_right')
+        )
+
+    join_cols = join_cols or []
+    
+    base_cols_1 = [col for col in df1.columns if col not in join_cols]
+    base_cols_2 = [col for col in df2.columns if col not in join_cols]
+
+    all_cols = list(set(base_cols_1 + base_cols_2))
+
+    for col in all_cols:
+        if col in join_cols:
+            continue
+        elif col in base_cols_1 and col in base_cols_2:
+            merged[f'{col}_diff'] = merged[f'{col}_left'] != merged[f'{col}_right']
+        elif col in base_cols_1:
+            merged[f'{col}_diff'] = True
+        elif col in base_cols_2:
+            merged[f'{col}_diff'] = True
+        else:
+            raise ValueError(f'Column {col} not found in either DataFrame')
+        
+    # Sort ONLY the Diff Columns, leaving the original columns in the order they were
+    original_cols = sorted([col for col in merged.columns if '_diff' not in col])
+    diff_cols = sorted([col for col in merged.columns if '_diff' in col])
+
+    merged = merged.reindex(columns=original_cols + diff_cols)
+
+    return merged[merged[[f'{col}_diff' for col in all_cols]].any(axis=1) == True]
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from streamlit_ace import st_ace
 
 from src.load_handlers import load_data
-from src.comparisons import compare_all
+from src.comparisons import compare_all, compare_row_by_row
 
 st.set_page_config(page_title='Synonym', page_icon=':ghost:', layout='wide', initial_sidebar_state='auto')
 
@@ -45,25 +45,12 @@ def compare_datasets(key1: str, key2: str) -> None:
     df1 = st.session_state.get(f'df__{key1}')
     df2 = st.session_state.get(f'df__{key2}')
 
-    # groupby_columns = st.checkbox('Group by columns?', key=f'groupby_columns__{key1}_{key2}')
-
-    # if groupby_columns and df1 is not None and df2 is not None:
-    #     col_left, _, col_right = st.columns([5,1,5])
-    #     groupby_left = col_left.multiselect('Group by (Left Dataset)', df1.columns.tolist(), key=f'groupby_left__{key1}')
-    #     groupby_right = col_right.multiselect('Group by (Right Dataset)', df2.columns.tolist(), key=f'groupby_right__{key2}')
-
-    #     if groupby_left and groupby_right:
-    #         df1 = df1.groupby(groupby_left)
-    #         df2 = df2.groupby(groupby_right)
-
-    #         print(df1.head())
-    #         print(df2.head())
-
-    #         col_left.write('Grouped Data')
-    #         col_left.write(df1.head())
-
-    #         col_right.write('Grouped Data')
-    #         col_right.write(df2.head())
+    provide_join_on_cols = st.checkbox('Provide columns to join on?', key='join_on_cols_checkbox')
+    if provide_join_on_cols:
+        all_cols = set(df1.columns).union(set(df2.columns))
+        join_cols = st.multiselect('Columns to join on', list(all_cols), key='join_on_cols_multiselect')
+    else:
+        join_cols = None
 
     if st.button('Compare datasets'):
         if df1 is not None and df2 is not None:
@@ -86,24 +73,30 @@ def compare_datasets(key1: str, key2: str) -> None:
 
             for comparison in all_comparisons:
                 with st.container():
+                    st.write(f'#### {comparison["metric"]}')
                     cola, _, colb = st.columns([5,1,5])
 
-                    cola.write(f'**{comparison["metric"]}**')
-                    cola.write(comparison['result1'])
-
-                    colb.write(f'**{comparison["metric"]}**')
-                    colb.write(comparison['result2'])
+                    try:
+                        cola.dataframe(comparison['result1'], use_container_width=True)
+                    except Exception:
+                        cola.write(comparison['result1'])
+                    
+                    try:
+                        colb.dataframe(comparison['result2'], use_container_width=True)
+                    except Exception:
+                        colb.write(comparison['result2'])
 
                     res = comparison['comparison']
                     if res:
                         st.write(f'Result: :white_check_mark: {res}')
                     else:
                         st.write(f'Result: :x: {res}')
+                    st.write(comparison['explanation'])
                     st.write('---')
 
             st.subheader('Row by Row Comparison')
-            comparsion = df1.compare(df2, result_names=('Left', 'Right'), align_axis=1)
-            st.write(comparsion)
+            comparsion = compare_row_by_row(df1, df2, join_cols=join_cols)
+            st.dataframe(comparsion, use_container_width=True)
         else:
             st.error('Please load both datasets before comparing.')
 
